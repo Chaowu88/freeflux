e8b4ec27f227432afe717aa7b85a86e81280588a
Chao Wu <54419976+Chaowu88@users.noreply.github.com>
2022-11-27 18:48:21 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,11 +8,11 @@ sys.path.insert(0, SRC_PATH)
 
 
 # ------------------------ General configuration ------------------------
-extensions = ['sphinx.ext.duration',
-              'sphinx.ext.doctest',
-              'sphinx.ext.autodoc',
-              'sphinx.ext.autosummary',
+extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.intersphinx',
+              'sphinx.ext.mathjax',
+              'sphinx.ext.viewcode',
+              'sphinx.ext.autosummary',
               'nbsphinx',
               'autoapi.extension']
 
@@ -20,21 +20,17 @@ autoapi_type = 'python'
 autoapi_dirs = [join(SRC_PATH, 'freeflux')]
 
 master_doc = 'index'
-
-# General information about the project
-project = 'freeflux'
-copyright = '2022, Chao Wu'
-author = 'Chao Wu'
-
-from freeflux import __version__ as version
-
-release = version
-# version = '0.3.0'
-# release = '0.3.0'
-
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 exclude_patterns = ['_build']
+
+
+# ------------------------ Project information ------------------------
+project = 'freeflux'
+copyright = '2022, Chao Wu'
+author = 'Chao Wu'
+from freeflux import __version__ as version
+release = version
 
 pygments_style = 'sphinx'
 
@@ -43,13 +39,35 @@ pygments_style = 'sphinx'
 import sphinx_rtd_theme
 
 html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+
+# ------------------------ Options for LaTeX output ------------------------
+# Grouping the document tree into LaTeX files. List of tuples
+# (source start file, target name, title, author, 
+#  documentclass [howto, manual, or own class]).
+latex_documents = [(master_doc, 
+                    project+'.tex', 
+                    project+' Documentation',
+                    author,
+                    'manual')]
+
+
+# ------------------------ Options for manual page output ------------------------
+# One entry per manual page. List of tuples
+# (source start file, name, description, authors, manual section).
+man_pages = [(master_doc, 
+              project, 
+              project+' Documentation',
+              [author], 
+              1)]
 
 
 # ------------------------ Options for Texinfo output ------------------------
 # Grouping the document tree into Texinfo files. List of tuples
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
-texinfo_documents = [('index',
+texinfo_documents = [(master_doc,
                       project,
                       project+' Documentation',
                       author,
